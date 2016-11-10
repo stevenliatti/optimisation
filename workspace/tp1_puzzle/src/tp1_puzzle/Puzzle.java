6445f26a242e097cfc6d929a9eefc729fd0710ff
steven.liatti <steven.liatti@etu.hesge.ch>
2016-11-10 21:54:22 +0100

    Fini puzzle, tout fusionné dans la fonction search

--- workspace/tp1_puzzle/src/tp1_puzzle/Puzzle.java
+++ workspace/tp1_puzzle/src/tp1_puzzle/Puzzle.java
@@ -8,13 +8,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Puzzle {
 
+	enum Search {BLIND, SQUARE_FALSE, MANHATTAN;}
+
 	final static int SMALL_PUZZLE = 8;
 	final static int BIG_PUZZLE = 15;
 
 	public static void main(String[] args) {
 		final int[][] canonical8 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 		final int[][] canonical15 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
-		final int LIMIT = 200000;
+		final int LIMIT = 300000;
 		final int[][] canonical;
 
 		State startState;
@@ -33,7 +35,7 @@ public class Puzzle {
 			System.out.println("État initial [1 2 3 ...] : ");
 			str = sc.nextLine();
 			startState = State.stateFromString(str);
-			
+
 			if (finalState.getSizePuzzle() != startState.getSizePuzzle()) {
 				throw new IllegalArgumentException("La taille de l'état initial diffère ce celle de l'état final.");
 			}
@@ -69,33 +71,48 @@ public class Puzzle {
 
 		switch (str) {
 		case "1":
-			blindSearch(startState, finalState, sizePuzzle, LIMIT, false);
+			search(startState, finalState, sizePuzzle, LIMIT, false, Search.BLIND);
 			break;
 		case "2":
 			if (sizePuzzle == SMALL_PUZZLE) {
-				blindSearch(startState, finalState, sizePuzzle, LIMIT, true);
+				search(startState, finalState, sizePuzzle, LIMIT, true, Search.BLIND);
 			}
 			else {
 				throw new IllegalArgumentException("Vous ne pouvez faire une recherche avec états visités avec une taille > 8");
 			}
 			break;
 		case "3":
-			heuristicSearch(startState, finalState, sizePuzzle, LIMIT, false);
+			search(startState, finalState, sizePuzzle, LIMIT, true, Search.SQUARE_FALSE);
 			break;
 		case "4":
-			heuristicSearch(startState, finalState, sizePuzzle, LIMIT, true);
+			search(startState, finalState, sizePuzzle, LIMIT, true, Search.MANHATTAN);
 			break;
 		default:
-			blindSearch(startState, finalState, sizePuzzle, LIMIT, false);
+			search(startState, finalState, sizePuzzle, LIMIT, false, Search.BLIND);
 			break;
 		}
 	}
 
-	public static void blindSearch(State startState, State finalState, int sizePuzzle, int limit, boolean visited) {
+	public static void search(State startState, State finalState, int sizePuzzle, int limit, boolean visited, Search search) {
 		State currentState = new State(startState);
 		int iterations = 0;
-		ConcurrentLinkedQueue<State> queue = new ConcurrentLinkedQueue<State>();
+		Queue<State> queue;
 		HashSet<State> visitedStates = new HashSet<State>();
+
+		switch (search) {
+		case BLIND:
+			queue = new ConcurrentLinkedQueue<State>();
+			break;
+		case SQUARE_FALSE:
+			queue = new PriorityQueue<State>(new SquareFalse(finalState));
+			break;
+		case MANHATTAN:
+			queue = new PriorityQueue<State>(new Manhattan(finalState));
+			break;
+		default:
+			queue = new ConcurrentLinkedQueue<State>();
+			break;
+		}
 
 		// 1. Si SOLUTION?(état-initial) alors retourner état-initial
 		if (currentState.equals(finalState)) {
@@ -152,71 +169,6 @@ public class Puzzle {
 						queue.add(state);
 						visitedStates.add(state);
 					}
-				}
-			}
-		}
-	}
-
-	public static void heuristicSearch(State startState, State finalState, int sizePuzzle, int limit, boolean manhattan) {
-		State currentState = new State(startState);
-		int iterations = 0;
-		Queue<State> queue;
-		if (manhattan) {
-			queue = new PriorityQueue<State>(new Manhattan(finalState));
-		}
-		else {
-			queue = new PriorityQueue<State>(new SquareFalse(finalState));
-		}
-		HashSet<State> visitedStates = new HashSet<State>();
-
-		// 1. Si SOLUTION?(état-initial) alors retourner état-initial
-		if (currentState.equals(finalState)) {
-			System.out.println("Trouvé !");
-			State.printSituation(startState, currentState, iterations);
-			return;
-		}
-
-		// 2. INSERER(noeud-initial,FILE)
-		queue.add(currentState);
-		visitedStates.add(currentState);
-
-		// 3. Répéter:
-		while (true) {
-			// a. Si vide(FILE) alors retourner échec
-			if (queue.isEmpty()) {
-				System.out.println("Échec");
-				State.printSituation(startState, currentState, iterations);
-				return;
-			}
-
-			if (sizePuzzle == BIG_PUZZLE) {
-				// Si la recherche dépasse un certain seuil
-				if (queue.size() >= limit) {
-					System.out.println("Seuil fixé (" + limit + ") dépassé");
-					State.printSituation(startState, currentState, finalState, iterations);
-					return;
-				}
-			}
-
-			// b. n <- RETIRER(FILE)
-			// c. s <- ETAT(n)
-			currentState = queue.poll();
-			iterations++;
-			System.out.println("--- " + iterations + " ---");
-			System.out.println(currentState);
-			// i.  Créer un nouveau noeud n' comme "enfant" de n
-			// ii. Si SOLUTION?(s') alors retourner chemin ou état-solution
-			if (currentState.equals(finalState)) {
-				System.out.println("Trouvé !");
-				State.printSituation(startState, currentState, iterations);
-				return;
-			}
-			currentState.generateSuccessors();
-			for (State state : currentState.getSuccessors()) {
-				// iii. INSERER(n',FILE)
-				if (!visitedStates.contains(state)) {
-					queue.add(state);
-					visitedStates.add(state);
 				}
 			}
 		}
