bd8fd72aab709f832854eff05ed828fa5cc0952b
steven.liatti <steven.liatti@etu.hesge.ch>
2016-11-12 09:30:00 +0100

    Lecture d'un fichier sudoku OK, début initConstraint

---
+++ workspace/tp2_sudoku/src/tp2_sudoku/Square.java
@@ -0,0 +1,67 @@
+package tp2_sudoku;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Square {
+	private boolean taken;
+	private String value;
+	private Set<String> possibleValues;
+	
+	public Square() {
+		this.taken = false;
+		this.value = "-";
+		this.possibleValues = new HashSet<String>();
+		for (int i = 1; i <= Sudoku.SUDOKU_SIZE; i++) {
+			possibleValues.add(String.valueOf(i));
+		}
+	}
+	
+	public Square(String value) {
+		this.possibleValues = new HashSet<String>();
+		this.value = value;
+		
+		if (value.equals("-")) {
+			this.taken = false;
+			for (int i = 0; i < Sudoku.SUDOKU_SIZE; i++) {
+				possibleValues.add(String.valueOf(i));
+			}
+		}
+		else {
+			this.taken = true;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	public boolean isFree() {
+		return !taken;
+	}
+	
+	public boolean isTaken() {
+		return taken;
+	}
+	
+	public void setTaken(boolean taken) {
+		this.taken = taken;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Set<String> getPossibleValues() {
+		return possibleValues;
+	}
+	
+	public void setPossibleValues(Set<String> possibleValues) {
+		this.possibleValues = possibleValues;
+	}
+}
--- workspace/tp2_sudoku/src/tp2_sudoku/Sudoku.java
+++ workspace/tp2_sudoku/src/tp2_sudoku/Sudoku.java
@@ -1,10 +1,57 @@
 package tp2_sudoku;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Sudoku {
+	
+	public final static int SUDOKU_SIZE = 9;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Square[][] board = boardFromFile("data/sudoku.txt");
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
+		initConstraint(board);
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
+	}
 
+	public static Square[][] boardFromFile(String fileName) throws FileNotFoundException, IOException {
+		Square[][] board = new Square[SUDOKU_SIZE][SUDOKU_SIZE];
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String line = null;
+		String[] split;
+		int i = 0;
+		while ((line = br.readLine()) != null) {
+			split = line.split(" ");
+			for (int j = 0; j < SUDOKU_SIZE; j++) {
+				board[i][j] = new Square(split[j]);
+			}
+			i++;
+		}
+
+		br.close();
+		return board;
+	}
+	
+	public static void initConstraint(Square[][] board) {
+		for (int i = 0; i < SUDOKU_SIZE; i++) {
+			for (int j = 0; j < SUDOKU_SIZE; j++) {
+				if (board[i][j].isFree()) {
+					
+				}
+			}
+		}
 	}
 
 }
