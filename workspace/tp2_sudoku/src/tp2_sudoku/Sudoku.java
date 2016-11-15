f85c44d01aba12cc69051391f877ac3c1a5836d6
steven.liatti <steven.liatti@etu.hesge.ch>
2016-11-15 14:49:36 +0100

    Ajout des commentaires pour Sudoku.java. Faudrait terminer LeL

--- workspace/tp2_sudoku/src/tp2_sudoku/Sudoku.java
+++ workspace/tp2_sudoku/src/tp2_sudoku/Sudoku.java
@@ -1,120 +1,67 @@
 package tp2_sudoku;
 
-import java.awt.Point;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Classe principale du programme. Construit un sudoku à partir
+ * d'un fichier et le résout.
+ * 
+ * @author Steven Liatti
+ */
 public class Sudoku {
 
 	static boolean FOUND = false;
 
+	/**
+	 * Point d'entrée du programme.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		Board board = new Board("data/sudoku.txt");
 		System.out.println(board);
-		System.out.println("-------------------------------------------------");
+		System.out.println("-----------------------");
 		Board solution = resolve(board);
 		System.out.println(solution);
 	}
 
+	/**
+	 * Fonction implémentant l'algorithme de backtracking.
+	 * 
+	 * @param board
+	 * @return
+	 */
 	public static Board resolve(Board board)
 	{
+		// 1. Si assignement A est complet alors retourner A
 		if(board.isFull()) {
 			return board;
 		}
 
+		//2. X <- sélectionner une variable absente de A
+		//3. D <- sélectionner un ordre sur le domaine de X
 		Board newBoard = new Board(board);
 		Square bestSquare = newBoard.getBestSquare();
 		List<String> values = bestSquare.getLegalValues();
 
+		// 4. Pour chaque valeur v dans D faire
 		for (int i = 0; i < values.size(); i++) {
-
-//			bestSquare.setValue(values.get(i));
+			// a. Add (X <- v) à A
+			// b. var-domaines <- forward checking(var-domaines, X, v, A)
+			// c. Si aucune variable a une domaine vide alors
+			// i. résultat <- PSC-BACKTRACKING(A, var-domaines)
 			newBoard.update(bestSquare, values.get(i));
 			Board solution = resolve(newBoard);
 
+			// ii. Si résultat != échec alors retourner résultat
 			if(solution != null) {
 				return solution;
 			}
 		}
 
+		// 5. Retourner échec
 		return null;
-	}
-
-	//	public static void resolve(Board board) {
-	//		if (!FOUND) {
-	//			for (Entry<Integer, List<Point>> entry : board.getBestSquares().entrySet()) {
-	//				if (!FOUND) {
-	//					for (Point point : entry.getValue()) {
-	//						Square square = board.getBoard()[point.x][point.y];
-	//						if (!FOUND) {
-	//							for (String value : square.getLegalValues()) {
-	//								if (!FOUND) {
-	//									if (check(board, point, value)) {
-	//										Board newBoard = update(board, point, value);
-	//										System.out.println(newBoard);
-	//										if (newBoard.isFull()) {
-	//											System.out.println(newBoard);
-	//											FOUND = true;
-	//										}
-	//										else {
-	//											resolve(newBoard);
-	//										}
-	//									}
-	//								}
-	//							}
-	//						}
-	//					}
-	//				}
-	//			}
-	//		}
-	//		else {
-	//			System.out.println(board);
-	//		}
-	//	}
-
-	private static boolean check(Board board, Point point, String value) {
-		int modRow = (point.x / Board.SQUARE_LATIN_SIZE) * Board.SQUARE_LATIN_SIZE;
-		int modColumn = (point.y / Board.SQUARE_LATIN_SIZE) * Board.SQUARE_LATIN_SIZE;
-
-		for (int i = 0; i < board.getSize(); i++) {
-			if (board.getBoard()[i][point.y].getValue().equals(value))
-				return false;
-			if (board.getBoard()[point.x][i].getValue().equals(value))
-				return false;
-		}
-
-		for (int i = modRow; i < modRow + Board.SQUARE_LATIN_SIZE; i++) {
-			for (int j = modColumn; j < modColumn + Board.SQUARE_LATIN_SIZE; j++) {
-				if (board.getBoard()[i][j].getValue().equals(value))
-					return false;
-			}
-		}
-
-		return true;
-	}
-
-	private static Board update(Board board, Point point, String value) {
-		Board newBoard = new Board(board);
-
-		for (int i = 0; i < newBoard.getSize(); i++) {
-			newBoard.getBoard()[i][point.y].updateLegalValues(value);
-			newBoard.getBoard()[point.x][i].updateLegalValues(value);
-		}
-
-		int modRow = (point.x / Board.SQUARE_LATIN_SIZE) * Board.SQUARE_LATIN_SIZE;
-		int modColumn = (point.y / Board.SQUARE_LATIN_SIZE) * Board.SQUARE_LATIN_SIZE;
-
-		for (int i = modRow; i < modRow + Board.SQUARE_LATIN_SIZE; i++) {
-			for (int j = modColumn; j < modColumn + Board.SQUARE_LATIN_SIZE; j++) {
-				newBoard.getBoard()[i][j].updateLegalValues(value);
-			}
-		}
-
-		newBoard.getBoard()[point.x][point.y].setValue(value);
-		newBoard.getBoard()[point.x][point.y].setTaken(true);
-		newBoard.reduceSquareFree();
-		newBoard.findBestsSquare();
-
-		return newBoard;
 	}
 }
